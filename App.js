8bfce3badeb4f322f8b0723ca9e42df70a26d425
mauryavn76 <51995096+mauryavn76@users.noreply.github.com>
2019-07-23 11:52:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-// class App extends hello mukesh Component {
+// class App extends Component {
 //   render() {
 //     return (
 //       <div className="App">
