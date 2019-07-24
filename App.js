4bf196126cc9ff420c705b4dcb8ff248ddc03c68
mauryavn76 <51995096+mauryavn76@users.noreply.github.com>
2019-07-24 13:50:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-// class App extends hello muku Component {
+// class App extends Component {
 //   render() {
 //     return (
 //       <div className="App">
